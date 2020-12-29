8eb0532f0cdd69b6bc7f75f5b4462c0785583c8e
GeanCarloV <gean.carlo.vitorino@gmail.com>
2020-12-29 14:05:18 -0600

    proyecto de prueba

---
+++ fronted/src/components/About.js
@@ -0,0 +1,65 @@
+import React, { Fragment } from "react";
+
+export const About = () => (
+  <Fragment>
+    <div className="row">
+      <h3>About</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        consequuntur, animi modi aperiam eos doloribus error temporibus ducimus,
+        odio aut cum numquam dolore minus tempora quibusdam hic provident beatae
+        totam dignissimos placeat tenetur nisi minima voluptatum! Quae
+        voluptatum qui deleniti incidunt fuga, delectus eius quam soluta ipsa
+        dolorem alias inventore, ad dolor mollitia eum fugit, rem ex! Aperiam
+        excepturi, quo consectetur sapiente possimus vero nisi perferendis
+        dolore voluptate doloribus quidem distinctio sint, architecto provident
+        repellendus quod odio animi. Magnam eius, eligendi, veritatis dolor,
+        incidunt culpa harum tenetur suscipit tempora sequi non ab. Mollitia
+        fugiat expedita molestiae quae. Repellendus ad ab fugiat deleniti
+        aspernatur, facilis necessitatibus officiis, hic expedita saepe quae
+        distinctio eligendi fugit at, cumque illo molestias suscipit!
+        Reiciendis, nihil eaque quis pariatur repellendus, illum consequatur,
+        unde cumque vero natus ipsum! Distinctio nesciunt possimus quibusdam
+        amet quaerat sapiente eius cumque fuga doloremque illo quos odit
+        tempora, vero est omnis esse.
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        facere corporis nihil dicta ullam! Qui nobis iusto quas officiis
+        corporis voluptatibus harum? Dolorum at doloremque similique, odit vero
+        perferendis? Expedita. Numquam perspiciatis repudiandae, consequatur
+        ipsa totam inventore, delectus eius dolor harum adipisci esse quaerat.
+        Iusto pariatur aliquid maiores id dolorem quis dignissimos qui quasi
+        deserunt maxime sunt, quisquam culpa quidem. Officiis fugit nam
+        laboriosam maxime sit dolor voluptatem explicabo necessitatibus
+        consequuntur nobis non reiciendis delectus quod, et architecto totam
+        fugiat magni temporibus eos, nemo sunt porro vel recusandae ex!
+        Aspernatur. Hic minus enim, distinctio esse tempore doloribus nesciunt
+        officiis impedit vero minima alias qui magnam adipisci earum tenetur
+        corrupti perferendis ullam dolor. Commodi fugit magnam ullam fuga
+        architecto dolores obcaecati? Minima esse dicta natus culpa ea
+        reiciendis veniam quibusdam, voluptas voluptatum veritatis quae itaque
+        quisquam repellendus adipisci harum dolorem magni consequatur voluptate
+        accusantium non vitae aliquam id error suscipit. Autem. Delectus laborum
+        eos amet numquam! Nisi assumenda repudiandae mollitia cupiditate enim in
+        molestias commodi dolore odit incidunt reprehenderit, esse amet maiores
+        dolorem vitae rerum fuga! Deserunt iste excepturi laborum consequuntur?
+        Assumenda alias iure doloribus reiciendis obcaecati, possimus nesciunt
+        incidunt dolorum, natus esse asperiores laborum at, exercitationem
+        blanditiis dolore magnam! Ratione amet ullam in animi quas. Aspernatur
+        ab eveniet perspiciatis qui. Sed quo velit, cum ipsum quod provident est
+        sint nobis deleniti culpa. Expedita repellendus ex obcaecati nam
+        nostrum, consectetur quo, ducimus officia facere, harum corporis
+        voluptate laborum ratione commodi autem! Deserunt non velit minima nam
+        delectus maiores cupiditate sunt id voluptas, at necessitatibus dolor
+        quasi natus, illo totam harum possimus, quibusdam ullam. Reiciendis
+        saepe, omnis ullam soluta dignissimos eos laborum. At sit dolorem qui
+        culpa ea tenetur voluptatum ratione. Alias reiciendis fuga, placeat
+        quaerat laboriosam excepturi perferendis reprehenderit laborum corrupti
+        asperiores et illum? Dolores sit ducimus facilis nobis! Dignissimos,
+        ratione!
+      </p>
+    </div>
+  </Fragment>
+);
